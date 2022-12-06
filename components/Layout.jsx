728b968ae6f54616e4c65273168bc28274966bff
Matheus Ghignatti <matheusghignatti@gmail.com>
2022-12-05 22:09:41 -0300

    add Layout, Navbar, Footer

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-const layout = () => {
-  return <div>layout</div>;
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Boats Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
-export default layout;
+export default Layout;
